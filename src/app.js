935bdcca27bf570390f8e80c73c32d55e5d3893f
Cristian P <wombo17@gmail.com>
2019-05-07 14:38:29 -0600

    Adding 404 error page
    Adding 404 error page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
@@ -22,20 +22,27 @@ const EditExpensePage = () => (
   </div>
 );
 
-const HelpExpensePage = () => (
+const HelpPage = () => (
   <div>
     This is from my help expense component
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage}/>
       <Route path="/edit" component={EditExpensePage}/>
-      <Route path="/help" component={HelpExpensePage}/>
-    </div>
+      <Route path="/help" component={HelpPage}/>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
